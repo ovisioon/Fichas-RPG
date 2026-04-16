67019ff528b05168cf41c7056943975285811603
vision <100390164+ovisioon@users.noreply.github.com>
2026-04-16 02:31:59 -0300

    Update header text to 'VICTOR E MAC SÃO CORNES'

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -35,7 +35,7 @@ export function Header({ displayName, onBack, showBack = false }: HeaderProps) {
 
       {/* Logo "Oitavo B" com efeito glitch */}
       <h1 className="glitch-text text-center" data-text="VICTOR E MAC SÃO CORNES">
-        OITAVO B
+        VICTOR E MAC SÃO CORNES
       </h1>
 
       {/* Área do usuário/logout */}
